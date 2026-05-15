886359971229cf69b180832d53578b248373a31a
Samuel <araujosamuel352@gmail.com>
2026-05-15 00:40:06 -0300

    tabela automatica e debug False

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
+# ── Cria tabelas automaticamente ao iniciar ──
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -45,4 +49,4 @@ def contato():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug = True)
+    app.run(debug = False)
